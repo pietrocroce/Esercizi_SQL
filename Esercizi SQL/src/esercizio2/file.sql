cf839c13c5e3fc4238baec5d0ae9cc201eabe491
Pietro_Croce <pcroce91@gmail.com>
2023-06-14 16:57:23 +0200

    CORRETTI ESERCIZI, manca il 7 da vedere la queri nascosta

--- Esercizi SQL/src/esercizio2/file.sql
+++ Esercizi SQL/src/esercizio2/file.sql
@@ -4,15 +4,27 @@
  -- Beef Patty
  -- Lettuce
  -- Yoghurt
+ --L'HO FATTO INSIEME AL PUNTO DUE.
 
   -- Write a SELECT statement to select the NAME of all the ingredients.
- SELECT NAME FROM INGREDIENT
+
+ SELECT NAME FROM INGREDIENT;
+
   -- Write a SELECT statement to select the NAME and the PRICE of the ingredients
- SELECT NAME AND PRICE FROM INGREDIENT
+
+  CREATE TABLE INGREDIENT_WITH_PRICE(NAME VARCHAR(255), PRICE DOUBLE);
+
+  INSERT INTO INGREDIENT_WITH_PRICE(NAME, PRICE) VALUE (Eggs, 1.10),(Eggs, 1.10),(Bread, 1.50),(Beef Patty, 3.20),(Lettuce, 0.99),(Yoghurt, 1.40);
+
+ SELECT NAME AND PRICE FROM INGREDIENT;
+
   -- Write a SELECT statement to select all columns of ingredients, ordered by NAME
+
  SELECT * FROM INGREDIENT
  ORDER BY NAME
+
   -- Write a SELECT statement to select all columns of ingredients,
   -- ordered by "GLUTEN_FREE" with the "true" items on top
+
  SELECT * FROM INGREDIENT
- ORDER BY GLUTEN FREE = TRUE ASC
+ ORDER BY GLUTEN FREE = TRUE DESC;
--- Esercizi SQL/src/esercizio4/query.sql
+++ Esercizi SQL/src/esercizio4/query.sql
@@ -30,28 +30,3 @@ CREATE TABLE INGREDIENT(
   INSERT INTO INGREDIENT(NAME, COINTAIN_LACTOSE, VEGETARIAN, VEGAN, GLUTEN_FREE)
   VALUES('Yoghurt',true,true,false,true);
 
- -- Using a CREATE TABLE AS statement, create a new table called 'CHEAP_MEALS'
- -- that contains all meals with a price less than €15.
-  CREATE TABLE CHEAP_MEAL AS
-  SELECT * FROM MEAL WHERE PRICE < 15;
-   -- Using an INSERT statement with a SELECT statement, insert all ingredients that are vegan into
-  -- a new table called 'VEGAN_INGREDIENT', you can create an empty clone of a table with the following code:
- -- CREATE TABLE VEGAN_INGREDIENT AS
- -- SELECT * FROM INGREDIENT WHERE FALSE
- CREATE TABLE VEGAN_INGREDIENT AS
- SELECT * FROM INGREDIENT WHERE VEGAN = FALSE;
-
- INSERT INTO VEGAN_INGREDIENT
- SELECT* FROM INGREDIENT WHERE VEGAN = TRUE;
- -- Using a CREATE VIEW statement, create a view called 'EXPENSIVE_MEAL' that shows
- -- all meals with a price greater than €15.
- CREATE VIEW EXPENSIVE_MEAL AS
- SELECT * FROM MEAL WHERE PRICE > 15;
- -- Using a CREATE VIEW statement, create a view called 'LACTOSE_FREE_INGREDIENT'
- -- that shows all any ingredients without lactose.
- CREATE VIEW LACTOSE_FREE_INGREDIENT AS
- SELECT * FROM INGREDIENT WHERE COINTAIN_LACTOSE = FALSE;
- -- Using a CREATE TABLE AS statement, create a new table called 'HIGH_CALORIE_MEAL'
- -- that contains all meals with more than 650 calories.
- CREATE TABLE HIGH_CALORIE_MEAL AS
- SELECT * FROM MEAL WHERE CALORIES > 650;
